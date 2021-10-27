f968d38d0bb6eacf28b3b0ccf94fe1b37fb46251
Moises <moises.ximenes5@gmail.com>
2021-10-27 14:15:32 -0300

    Subindo arquivo de Módulos

---
+++ Módulos/main.mjs
@@ -0,0 +1,6 @@
+import { mostraCidade, mostraIdade, mostraHobby } from "./funcoes.mjs";
+
+
+console.log(mostraIdade("Moisés", 20));
+console.log(mostraCidade("Moisés", "Campo Grande"));
+console.log(mostraHobby("Moisés", "ler livros"));
